920ed112a2e87a720d961e3c92fae6f3108f92ba
Akhilesh-Chandrakar <akhil79022@gmail.com>
2023-01-11 00:21:42 +0530

    made some changes

--- pages/feed/[pno].js
+++ pages/feed/[pno].js
@@ -3,13 +3,18 @@ import { useRouter } from 'next/router';
 import Navbar from '../../components/Navbar'
 import styles from '../../styles/Feed.module.css';
 export const Feed = ({ articles, pageNumber }) => {
+  //receive articles as object from the api from server side rendering and pageNumber is given by the user
   const router = useRouter();
+  //if articles exust
   return  articles.length? (
     <>
       <div className='bg-slate-100 page-container'>
         <Navbar/>
+        
     <div className=" flex mt-24 items-center flex-col justify-center">
+    
     {articles.map((article, index) => (
+      // using arrays. map to destructue
       <div key={index} className="w-96 mb-10 pb-10 rounded-r-lg">
         <h1 className='font-bold text-xl cursor-pointer text-center mb-2 pb-2' onClick={() => (window.location.href = article.url)}>{article.title}</h1>
         <p className='text-center mb-2 pb-2'>{article.description}</p>
@@ -18,15 +23,15 @@ export const Feed = ({ articles, pageNumber }) => {
     ))}
   </div>
      <div className={styles.paginator}>
+    
      <div
+       // adding previous and next for scrollto previous page and next page
        className={pageNumber === 1 ? styles.disabled : styles.active}
        onClick={() => {
          if (pageNumber > 1) {
            // As of the current version of Next.js the default behavior for router.push
            // will leave the scroll where it is, so we have to manually call scrollTo.
-           // This however is being worked on and is fixed in canary.
-           // Show this in tutorial vid:
-           // https://github.com/vercel/next.js/issues/3249
+           
            router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
          }
        }}
@@ -37,14 +42,12 @@ export const Feed = ({ articles, pageNumber }) => {
      <div>#{pageNumber}</div>
 
      <div
-       className={pageNumber === 10 ? styles.disabled : styles.active}
+       className={pageNumber === 5 ? styles.disabled : styles.active}
        onClick={() => {
-         if (pageNumber < 10) {
+         if (pageNumber < 5) {
            // As of the current version of Next.js the default behavior for router.push
            // will leave the scroll where it is, so we have to manually call scrollTo.
-           // This however is being worked on and is fixed in canary.
-           // Show this in tutorial vid:
-           // https://github.com/vercel/next.js/issues/3249
+           
            router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
          }
        }}
@@ -54,16 +57,23 @@ export const Feed = ({ articles, pageNumber }) => {
    </div>
    </div>
    </>
-  ): (<>
+  ): ( 
+  // if pageno is invalid since we have only 4 pages available from the api
+  // return no articles found
+  <>
+  
   <div className="bg-slate-100 page-container">
+
       <Navbar />
+      
       <div className=" flex mt-24 items-center flex-col justify-center">
         <h1 className='font-bold text-xl'>Oops! No articles for this page</h1>
       </div>
     </div>
     </>);
 };
-
+// get Serversideprops function provided by next.js which runs asynchronously at the server side and 
+//returns as props to the same component where it is defined
 export const getServerSideProps = async pageContext => {
   const pageNumber = pageContext.query.pno;
 
